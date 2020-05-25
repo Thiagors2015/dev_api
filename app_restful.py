53c872ab4af7f4f659d26b50c334bc2f0a391f39
Thiago Ramos <thiago.ramo.s@hotmail.com>
2020-05-25 09:01:51 -0300

    Inclusão da classe externa habilidades.py e a validação da existência ou não das habilidades antes do cadastro

--- app_restful.py
+++ app_restful.py
@@ -1,6 +1,6 @@
 from flask import Flask,request
 from flask_restful import Resource,Api
-from habilitades import Habilidades,Habilidade,validarHabilidades
+from habilidades import Habilidades,Habilidade,validarHabilidades
 import json
 app=Flask(__name__)
 api=Api(app)
@@ -55,7 +55,6 @@ api.add_resource(Desenvolvedor,'/dev/<int:id>')
 api.add_resource(Lista_Desenvolvedores,'/dev/')
 api.add_resource(Habilidades,'/habilidades/')
 api.add_resource(Habilidade,'/habilidades/<int:id>/')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
