3f2e080df5ab073d7be68e93fb2119d2cef0fc9d
Thiago Ramos <thiago.ramo.s@hotmail.com>
2020-05-25 08:47:22 -0300

    Inclusão da classe externa de habilidades e validação das habilidades previamente cadastradas

--- app_restful.py
+++ app_restful.py
@@ -1,6 +1,6 @@
 from flask import Flask,request
-from flask_restful import  Resource,Api
-from habilitades import Habilidades
+from flask_restful import Resource,Api
+from habilitades import Habilidades,Habilidade,validarHabilidades
 import json
 app=Flask(__name__)
 api=Api(app)
@@ -22,8 +22,13 @@ class Desenvolvedor(Resource):
         return response
     def put(self,id):
         dados= json.loads(request.data)
-        desenvolvedores[id]=dados
-        return dados
+        valid=validarHabilidades(dados)
+        if valid['status']==True:
+            desenvolvedores[id]=dados
+            return dados
+        else:
+            return {'status':'erro','menagem':'Habilidades não encontradas, faço o cadastro',
+                    'habilidades':valid['habilidades']}
     def delete(self,id):
         desenvolvedores.pop(id)
         return {'status':'sucesso registro excluído'}
@@ -31,19 +36,26 @@ class Desenvolvedor(Resource):
 class Lista_Desenvolvedores(Resource):
     def post(self):
         dados= json.loads(request.data)
-        posicao=len(desenvolvedores)
-        dados['id']=posicao
-        desenvolvedores.append(dados)
-        return desenvolvedores[posicao]
+        valid=validarHabilidades(dados)
+        if valid['status']==True:
+            posicao=len(desenvolvedores)
+            dados['id']=posicao
+            desenvolvedores.append(dados)
+            return desenvolvedores[posicao]
+        else:
+            return {'status':'erro','mensagem':'Habilidades não encontradas, faço o cadastro','habilidades':valid['habilidades']}
     def get(self):
         return desenvolvedores
+
+
+
 
 
 api.add_resource(Desenvolvedor,'/dev/<int:id>')
 api.add_resource(Lista_Desenvolvedores,'/dev/')
 api.add_resource(Habilidades,'/habilidades/')
-api.add_resource(Habilidades,'/habilidades/<hbAnt>/')
-api.add_resource(Habilidades,'/habilidades/<hbAnt>/<hbNova>/')
+api.add_resource(Habilidade,'/habilidades/<int:id>/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- habilitades.py
+++ habilitades.py
@@ -2,32 +2,68 @@ from flask_restful import Resource
 from flask import request
 import json
 
-lista_habilitades=[{'Python','Java'}]
-
+lista_habilitades=['Python','Java']
 class Habilidades(Resource):
     def get(self):
         return lista_habilitades
     def post(self):
-        dados=json.loads(request.data)
-        lista_habilitades.append(dados)
-    def put(self,hbAnt,hbNova):
-        posicao=lista_habilitades.index(hbAnt)
-        if posicao>=0:
-            lista_habilitades[posicao]=hbNova
-            mensagem='Habilidade {} alterada para {} com sucesso.'.format(hbAnt,hbNova)
-            resposta={'status':'sucesso','mensagem':mensagem}
+        dados = json.loads(request.data)
+        try:
+            pos = lista_habilitades.index(dados['habilidade'])
+        except ValueError:
+            pos=-1
+        if pos<0:
+            lista_habilitades.append(dados['habilidade'])
+            mensagem = 'Habilidade incluída com sucesso'
+            retorno = {'status': 'sucesso', 'mensagem': mensagem}
         else:
-            mensagem = 'Erro habilidade {} não foi encontrada'.format(hbAnt)
-            resposta = {'status': 'erro', 'mensagem': mensagem}
-        return resposta
-    def delete(self,hbAnt):
-        posicao = lista_habilitades.index(hbAnt)
-        if posicao >= 0:
-            lista_habilitades.pop(posicao)
-            mensagem = 'Habilidade {} excluída com sucesso.'.format(hbAnt)
-            resposta = {'status': 'sucesso', 'mensagem': mensagem}
-        else:
-            mensagem = 'Erro habilidade {} não foi encontrada'.format(hbAnt)
-            resposta = {'status': 'erro', 'mensagem': mensagem}
-        return resposta
+            mensagem = 'Habilidade já exite'
+            retorno = {'status': 'erro', 'mensagem': mensagem}
+        return retorno
 
+class Habilidade(Resource):
+    def delete(self,id):
+        try:
+            lista_habilitades.pop(id)
+            mensagem = 'Habilidade Excluída com sucesso'
+            retorno = {'status': 'sucesso', 'mensagem': mensagem}
+        except IndexError:
+            mensagem = 'Habilidade não foi encontrada'
+            retorno = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro não identificado'
+            retorno = {'status': 'erro', 'mensagem': mensagem}
+        return retorno
+    def put(self,id):
+        try:
+            dados = json.loads(request.data)
+            try:
+                pos = lista_habilitades.index(dados['habilidade'])
+            except ValueError:
+                pos = -1
+            if pos < 0:
+                lista_habilitades[id] = dados['habilidade']
+                mensagem = 'Habilidade alterada com sucesso'
+                retorno = {'status': 'sucesso', 'mensagem': mensagem}
+            else:
+                mensagem = 'Habilidade não foi alterada, habilidade informada já existe'
+                retorno = {'status': 'erro', 'mensagem': mensagem}
+        except IndexError:
+            mensagem = 'Habilidade não foi encontrada'
+            retorno = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro não identificado'
+            retorno = {'status': 'erro', 'mensagem': mensagem}
+        return retorno
+
+def validarHabilidades(dados):
+        achou=True
+        naoAchou=[]
+        for x in dados['habilidades']:
+            if x not in lista_habilitades:
+                naoAchou.append(x)
+                achou=False
+        if achou:
+            return {'status':achou}
+        else:
+            return {'status':achou,'habilidades':naoAchou}
